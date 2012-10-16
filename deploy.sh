9ba2a361b3ebcb7e85fb236c4dc0075dbd4776d8
Ricky Elrod <ricky@elrod.me>
2012-10-16 18:22:02 -0400

    Add gzip -9 of static assets to the deploy/build process.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-cd `dirname $0`
+#!/usr/bin/env bash
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $DIR
 git rm -rf staged start
 git commit -m "Deploying from upstream at: `date`"
 git clone git://github.com/breakpoint-eval/frontend
@@ -6,6 +8,10 @@ cp openshift.conf frontend/conf/
 
 pushd frontend
   mv conf/application.conf.dist conf/application.conf
+  for file in $(find ./app/assets/ -type f -print); do
+    gzip -c -9 $file > $file.gz
+    echo "[*] gzipping static assets."
+  done
   sbt clean update compile stage
 popd
 
@@ -17,3 +23,4 @@ rm -rf frontend
 git add .
 git commit --am -m "Deploying from upstream at: `date`"
 git push origin master
+popd
